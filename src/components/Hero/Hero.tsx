3e48d6a98353c567d367e4fb346570f943c10ad1
vitor lopes <vitorlopes@192.168.1.88>
2024-08-10 17:54:44 +0700

    changed image aproach to test something

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,35 +1,36 @@
-'use client'
+"use client";
 
-import React from 'react'
-import styles from "./Hero.module.css"
-import Image from 'next/image';
+import React from "react";
+import styles from "./Hero.module.css";
+import Image from "next/image";
 import { handleImageContextMenu } from "../../helper/functions";
-
 
 const Hero = () => {
   return (
     <div className={styles.container}>
       <h4 className={styles.featured}>FEATURED</h4>
       <div className={styles.imagesContainer}>
-        <Image
-          src="/images/hero.svg"
-          width={683}
-          height={205}
-          alt="hero logo"
-          className={styles.heroLogo}
-          onContextMenu={handleImageContextMenu}
-        />
-        <Image
-          src="/images/rekko.svg"
-          width={182}
-          height={182}
-          alt="hero image"
-          className={styles.heroImage}
-          onContextMenu={handleImageContextMenu}
-        />
+        <div className={styles.imageContainer}>
+          <Image
+            src="/images/hero.png"
+            alt="hero logo"
+            fill
+            className={styles.heroLogo}
+            onContextMenu={handleImageContextMenu}
+          />
+        </div>
+        <div className={styles.imageContainer2}>
+          <Image
+            src="/images/rekko.svg"
+            alt="hero image"
+            fill
+            className={styles.heroImage}
+            onContextMenu={handleImageContextMenu}
+          />
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Hero
+export default Hero;
